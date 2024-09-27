b3d05a741930323abc9d8f4343e3e639688ca324
Anna <jenova.anya@yandex.ru>
2024-09-28 02:06:29 +0300

    add homework-5

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,2 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920*1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void ChangeActionsTest () {
+    open("https://the-internet.herokuapp.com/drag_and_drop");
+    actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200, 0).release().perform();
+        $("#column-b").shouldHave(text("A"));
+    }
+    @Test
+    void ChangeElementTest () {
+        // - В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
+        $("#column-b").dragAndDrop(to($("#column-a")));
+        $("div#column-a").shouldHave(text("A"));
+    }
+
 }
