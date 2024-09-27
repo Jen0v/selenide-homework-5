8a6c016459342ad12d0326c246a04da6bf060877
Anna <jenova.anya@yandex.ru>
2024-09-28 00:56:43 +0300

    add GitHubEnterprizeTest

---
+++ src/test/java/GitHubEnterprizeTest.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+
+
+public class GitHubEnterprizeTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920*1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+    @Test
+    void searchEnterprizeTest() {
+        open("/");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(byLinkText("Enterprise")).click();
+        $(By.linkText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+
+    }
+}
